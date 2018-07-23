dc788bd2364b24cb9f4d4bd6c4bc345bbf4ae74a
Ctipsy <maplekonghou@gmail.com>
2018-07-23 11:21:54 +0800

    polish 我不是药神

--- 我不是药神/visualization_analysis.py
+++ 我不是药神/visualization_analysis.py
@@ -4,13 +4,16 @@ from pandas import DataFrame
 import re
 from pyecharts import Line, Geo, Bar, Pie, Page, ThemeRiver
 from snownlp import SnowNLP
+import jieba
+import matplotlib.pyplot as plt
+from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
 
 fth = open('pyecharts_citys_supported.txt', 'r', encoding='utf-8').read() # pyecharts支持城市列表
 
 # 过滤字符串只保留中文
 def translate(str):
     line = str.strip()
-    p2 = re.compile('[^\u4e00-\u9fa5]')  # 中文的编码范围是：\u4e00到\u9fa5
+    p2 = re.compile('[^\u4e00-\u9fa5]')   #中文的编码范围是：\u4e00到\u9fa5
     zh = " ".join(p2.split(line)).strip()
     zh = ",".join(zh.split())
     str = re.sub("[A-Za-z0-9!！，%\[\],。]", "", zh)
@@ -46,6 +49,45 @@ def draw_sentiment_pic(csv_file):
     line = Line(csv_file+":影评情感分析")
     line.add("", attr, val, is_smooth=True, is_more_utils=True)
     line.render(csv_file+"_情感分析曲线图.html")
+
+
+def word_cloud(csv_file, stopwords_path, pic_path):
+    pic_name = csv_file+"_词云图.png"
+    path = os.path.abspath(os.curdir)
+    csv_file = path+ "\\" + csv_file + ".csv"
+    csv_file = csv_file.replace('\\', '\\\\')
+    d = pd.read_csv(csv_file, engine='python', encoding='utf-8')
+    content = []
+    for i in d['content']:
+        try:
+            i = translate(i)
+        except AttributeError as e:
+            continue
+        else:
+            content.append(i)
+    comment_after_split = jieba.cut(str(content), cut_all=False)
+    wl_space_split = " ".join(comment_after_split)
+    backgroud_Image = plt.imread(pic_path)
+    stopwords = STOPWORDS.copy()
+    with open(stopwords_path, 'r', encoding='utf-8') as f:
+        for i in f.readlines():
+            stopwords.add(i.strip('\n'))
+        f.close()
+
+    wc = WordCloud(width=1024, height=768, background_color='white',
+                   mask=backgroud_Image, font_path="C:\simhei.ttf",
+                   stopwords=stopwords, max_font_size=400,
+                   random_state=50)
+    wc.generate_from_text(wl_space_split)
+    img_colors = ImageColorGenerator(backgroud_Image)
+    wc.recolor(color_func=img_colors)
+    plt.imshow(wc)
+    plt.axis('off')  
+    plt.show() 
+    wc.to_file(pic_name)
+
+
+
 
 def count_city(csv_file):
     path = os.path.abspath(os.curdir)
@@ -218,14 +260,17 @@ def score_draw(csv_file):
     page.render(csv_file[:-4] + "_日投票量分析汇总.html")
 
 
-def main(csv_file):
-    draw_sentiment_pic(csv_file)
-    draw_citys_pic(csv_file)
-    score_draw(csv_file)
+def main(csv_file, stopwords_path, pic_path):
+    # draw_sentiment_pic(csv_file)
+    # draw_citys_pic(csv_file)
+    # score_draw(csv_file)
+    word_cloud(csv_file,stopwords_path, pic_path)
 
 
 if __name__ == '__main__':
-    main("邪不压正")
+    # main("邪不压正", "stopwords_1.txt", "jiangwen.jpg")
+    main("我不是药神", "stopwords_2.txt", "xuzheng.jpg" )
+
 
 
 
